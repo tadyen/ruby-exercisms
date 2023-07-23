08e42b4b94194bbec82281313cfcde58361884d4
axwong <alexwong@tadyen.dev>
2023-07-23 15:13:40 +1000

    log-line-parser

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -3,15 +3,45 @@ class LogLineParser
     @line = line
   end
 
+  # Extract from format: '[<LOG>]: <Message>'
+  def match_a(line)
+    regex = /
+      \[ (?<log>\w+) \]:  # log-level
+      \s (?<msg>.+)       # error-message
+    /x
+    regex.match(line)
+  end
+
+  # Extract from format: '<Message> (<log>)'
+  def match_b(line)
+    regex = /
+      (?<msg>[\w\s]+)   # error-message
+      \( (?<log>\w+) \) # log-level
+    /x
+    regex.match(line)
+  end
+
   def message
-    raise 'Please implement the LogLineParser#message method'
+    match_a(@line)[:msg].strip
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    match_a(@line)[:log].strip.downcase
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    a = match_a(@line)
+    m = a || match_b(@line)
+    log = m[:log].strip
+    msg = m[:msg].strip
+    if a
+      "#{msg} (#{log.downcase})"
+    else
+      "[#{log.upcase}]: #{msg}"
+    end
   end
 end
+
+# puts LogLineParser.new('[ERROR]: Stack overflow').log_level
+# puts LogLineParser.new('Segmentation fault (error)').reformat
+# puts LogLineParser.new("[ERROR]: \t Corrupt disk\t \t \r\n").reformat
