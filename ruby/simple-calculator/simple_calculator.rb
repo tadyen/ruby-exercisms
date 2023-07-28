32f02bb3f24d7cb87849d10611b9217995d157d2
axwong <alexwong@tadyen.dev>
2023-07-28 18:20:42 +1000

    simple_calculator

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,27 @@
+
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
-  def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+  class UnsupportedOperation < StandardError
   end
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def self.calculate(num_a, num_b, operation)
+    raise UnsupportedOperation, 'Invalid operation' unless ALLOWED_OPERATIONS.include?(operation)
+    raise ArgumentError, 'Input must be numbers' unless [num_a, num_b].all? { |n| n.is_a?(Numeric) }
+
+    case operation
+    when '+'
+      "#{num_a} + #{num_b} = #{num_a + num_b}"
+    when '/'
+      return 'Division by zero is not allowed.' if num_b.zero?
+
+      "#{num_a} / #{num_b} = #{num_a / num_b}"
+    when '*'
+      "#{num_a} * #{num_b} = #{num_a * num_b}"
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
